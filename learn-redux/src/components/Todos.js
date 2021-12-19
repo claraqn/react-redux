063d5613921229e12773412c3d5c21f8f4fd2c83
claraqn <claraqn@naver.com>
2021-12-19 19:47:06 +0900

    React.memo에 대한 설명 추가

--- learn-redux/src/components/Todos.js
+++ learn-redux/src/components/Todos.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+// React.memo는 리렌더링을 방지해줌
+// 만약 컴포넌트가 같은 props를 받을 때 같은 결과를 렌더링한다면 React.memo를 사용하여 불필요한 컴포넌트 렌더링을 방지할 수 있다. 즉, 컴포넌트에 같은 props가 들어온다면 리액트는 컴포넌트 렌더링 과정을 스킵하고 마지막에 렌더링된 결과를 재사용한다.
+// React.memo는 오직 props가 변경됐는지 아닌지만 체크한다.
 const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
   return (
     <li
